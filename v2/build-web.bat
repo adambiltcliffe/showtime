740910a24fe011235c8fd5c74e971d7d2b940727
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2025-12-27 22:33:05 +0000

    added LOOK to links bar, added going to rooms by typing their names, added some more dialogue

--- v2/build-web.bat
+++ v2/build-web.bat
@@ -2,8 +2,8 @@
 del /s /q showtime_web\*
 rmdir showtime_web\resources
 rmdir showtime_web
-..\..\dialog-testbed\new\dialogc.exe -t aa showtime.dg debug.dg lib.dg
-rem ..\..\dialog-testbed\new\dialogc.exe -t aa showtime.dg lib.dg
+rem ..\..\dialog-testbed\new\dialogc.exe -t aa showtime.dg debug.dg lib.dg
+..\..\dialog-testbed\new\dialogc.exe -t aa showtime.dg lib.dg
 "..\..\dialog-testbed\choice test\prebuilt\win32\aambundle.exe" -o showtime_web showtime.aastory
 copy /y modweb\resources\* showtime_web\resources
 move showtime_web\play.html showtime_web\index.html
